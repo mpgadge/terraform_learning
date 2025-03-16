a1db0d53d600e93ec8946a507dd93706cdc9a603
Mohanish <mpgadge@gmail.com>
2025-03-16 20:10:21 +0530

    instance creation with security group and its rule code

---
+++ main.tf
@@ -0,0 +1,63 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web_monu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_monu.id]
+  
+
+  tags = {
+    Name = "HelloWorld_mohanish"
+  }
+}
+
+resource "aws_security_group" "web_monu" {
+  name = "web_monu"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_monu_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_monu.id
+}
+
+
+resource "aws_security_group_rule" "web_monu_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_monu.id
+}
+
+
+resource "aws_security_group_rule" "web_monu_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_monu.id
+}
